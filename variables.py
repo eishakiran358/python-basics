e7472e609c32a7eb717e4d9e5e131bbcd7e61001
Eisha kiran <eishakiranch358@gmail.com>
2026-06-29 09:02:18 +0500

    python variables
    Added examples and definitions of Python  variables and data types including string, integer, float, and boolean.

---
+++ variables.py
@@ -0,0 +1,69 @@
+#         ............ Variables ..........
+# A variable is a container that stores data or values in memory.
+# A variable is a named location in memory used to store data values.
+
+name = "Eishaa"
+age = 21
+cgpa = 3.54
+student = True
+
+#     .............  Vaiable datatypes..........
+
+# 1.String (str):
+# A string is a sequence of characters enclosed within single (' ') or double (" ") quotation marks.
+name = "Eishaa"
+city = "MULTAN"
+message = "Welcome to Python"
+
+print("String Examples:")
+print("Name:", name)
+print("City:", city)
+print("Message:", message)
+
+# 2. Integer (int):
+# An integer is a whole number without a decimal point.
+age = 21
+marks = 95
+year = 2026
+
+print("Integer Examples:")
+print("Age:", age)
+print("Marks:", marks)
+print("Year:", year)
+
+# 3. Float (float):
+# A float is a number that contains a decimal point.
+temperature = 36.5
+price = 999.99
+print("Float Examples:")
+print("CGPA:", cgpa)
+print("Temperature:", temperature)
+print("Price:", price)
+
+# 4. Boolean (bool):
+# A boolean data type represents one of two values: True or False.
+student = True
+has_passed = True
+is_married = False
+print("Boolean Examples:")
+print(" Student:", student)
+print("Has Passed:", has_passed)
+print("Is Married:", is_married)
+
+
+# Displaying values
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student Status:", student)
+
+# Updating a variable
+print("Updating Age...")
+age = 22
+print("New Age:", age)
+
+# Multiple variables
+city, country = "Karachi", "Pakistan"
+
+print("City:", city)
+print("Country:", country)
